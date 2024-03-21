7e65d4276a1bff1728134f18fe6cef947af601f4
ganiular <ganiular@gmail.com>
2024-03-21 17:09:11 +0100

    place holder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <div className='comming'>
+
+          <h1 className='comming__header'>COMMING SOON</h1>
+          <p className='comming__description'>Our website is under construction, follow us for update now!</p>
+          <div className='time-block'>
+            <div></div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
