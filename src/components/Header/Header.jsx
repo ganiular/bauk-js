fc485927266986db9184d8ce54bbcae33f2722b9
ganiular <ganiular@gmail.com>
2024-04-04 12:30:32 +0100

    design header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,20 @@
 import './Header.scss';
+import logo from '../../assets/logo/bauk-logo.jpg';
+
+function HeaderLogo() {
+    return <div className='header-logo'>
+        <img className='header-logo__image' src={logo} alt="Logo" />
+        <div className='header-logo__name'>
+            <div className='header-logo__school-name'>BAUK</div>
+            <div className='header-logo__brand-name'>Journal of Sociology</div>
+        </div>
+    </div>
+}
 
 function Header() {
-    return <header className='header'>Header</header>
+    return <header className='header'>
+        <HeaderLogo />
+    </header>
 }
 
 export default Header;
