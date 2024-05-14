99abaa39699d455163997bb75e5654e305787e41
ganiular <ganiular@gmail.com>
2024-05-14 17:21:45 +0100

    serve react static files

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const { errorHandler } = require('./handler.js');
+const path = require('path')
 
 const app = express();
 
 app.use(express.json());
 app.use(cors())
+app.use(express.static(path.join(__dirname, '..', 'build')))
+
+
 app.use('/auth', require('./routes/auth_routes.js'))
 app.use('/users', require('./routes/user_routes.js'))
+
+app.get('*', (req, res) => {
+    const indexPath = path.join(__dirname, '..', 'build', 'index.html')
+    console.log('Get: /');
+    res.sendFile(indexPath)
+})
 
 app.use(errorHandler)
 
