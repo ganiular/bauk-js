52c8bcae9f64ed90f0dd52088944bbb7968ca61d
ganiular <ganiular@gmail.com>
2024-05-14 07:51:46 +0100

    add cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { errorHandler } = require('./handler.js');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use('/auth', require('./routes/auth_routes.js'))
 app.use('/users', require('./routes/user_routes.js'))
 
